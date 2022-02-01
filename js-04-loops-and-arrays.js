db7766e23abc82657808ea0fac770dfe458152fc
Brian McClain <brianmccln@yahoo.com>
2022-02-01 16:48:30 -0500

    rename files

---
+++ js-04-loops-and-arrays.js
@@ -0,0 +1,21 @@
+// make an array of fruits
+var fruits = ['apple', 'blueberry', 'cherry', 'kiwi', 'lime', 'mango', 'peach', 'pear', 'pineapple', 'plum', 'raspberry', 'strawberry'];
+// use a loop to go through the array
+// for each item in the array, apply if else logic:
+// if the fruit includes 'berry', make jam: 'raspberry jam'
+// else if the fruit length (number of chars) is 5+, make jelly: 'cherry jelly'
+// else (the fruit length is only 4 letters), make jellybean: 'kiwi jellybean'
+// console log each thing as you go (each time through loop, another console.log)
+for(var i = 0; i < ???; i++) {
+
+    var product = "";
+    if(???) {
+        product = ??? + ' jam';
+    } else if(???) {
+        product = ??? + ' jelly';
+    } else {
+        product = ??? + ' jellybean';
+    }
+    console.log(product);
+
+}
